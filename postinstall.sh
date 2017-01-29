07b60fb47a31ce3f2d7035df1c73bc49735418a4
David Prock <dp@dprockcreative.com>
2017-01-28 16:42:21 -0800

    seperated out extraneuos css

--- postinstall.sh
+++ postinstall.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 if [ -w elevators ]; then
-  echo "./elevators is writable"
+  echo "/elevators is writable"
 else
   chmod +x elevators
-  echo "changed perms on ./elevators"
+  echo "changed perms on /elevators"
 fi
 
 echo "Building Elevator Directories"
 
-	mkdir -p elevators/css
-	mkdir -p elevators/fonts
-	mkdir -p elevators/images
-	mkdir -p elevators/js
+	mkdir -p /elevators/css
+	mkdir -p /elevators/fonts
+	mkdir -p /elevators/images
+	mkdir -p /elevators/js
 
 echo "Installing CSS"
 
-	cp -R ./src/css/ elevators/css
+	cp -R ./src/css/ /elevators/css
 
 echo "Installing Fonts"
 
-	cp -R ./src/fonts/ elevators/fonts
+	cp -R ./src/fonts/ /elevators/fonts
 
 echo "Installing Images"
 
-	cp -R ./src/images/	elevators/images
+	cp -R ./src/images/	/elevators/images
 
 echo "Installing JavaScript"
 
-	cp -R ./dist/ elevators/js
+	cp -R ./dist/ /elevators/js
